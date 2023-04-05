a25383cd099f8f2d951065d0388c78e4ea7870ac
mark.dnb@live.nl <mark.dnb@live.nl>
2023-04-05 10:12:42 +0200

    Put authguard back

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
   const user = useSupabaseUser()
-  // if (!user.value && to.path !== '/login') {
-  //   return navigateTo('/login')
-  // }
+  if (!user.value && to.path !== '/login') {
+    return navigateTo('/login')
+  }
 })
