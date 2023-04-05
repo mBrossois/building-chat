8f148839b3d98a61bb5a22d43a1e17bf25ab0da8
mark.dnb@live.nl <mark.dnb@live.nl>
2023-04-05 11:31:16 +0200

    updated privacy policy styling

--- pages/privacyPolicy.vue
+++ pages/privacyPolicy.vue
@@ -1,37 +1,47 @@
 <template>
-    <p>
+    <h3>
         Privacy Policy for Building chat pgb
-
+    </h3>
+    <p>
         At Building chat pgb, we are committed to protecting the privacy of our users. This Privacy Policy outlines the types of personal information we collect, how we use and share that information, and the choices you have about the information we collect.
+    </p>
+    <p>
 
         Information we collect:
-
+    </p>
+    <p>
         When you create an account, we collect your name and email address.
         When you use our chat app, we collect the messages you send and receive, and any media files you share.
         We may also collect information about the device you use to access our chat app, such as your device's unique identifier, operating system, and browser type.
         How we use your information:
-
+    </p>
+    <p>
         We use the information we collect to provide and improve our chat app and services.
         We may use your email address to send you important notices, such as changes to our Privacy Policy or Terms of Service.
         We may use your messages and media files to deliver them to the intended recipients, and to facilitate communication between you and other users of our chat app.
         We may use your device information to improve the performance and security of our chat app.
         Sharing your information:
-
+    </p>
+    <p>
         We may share your information with third-party service providers who assist us in providing our chat app and services, such as hosting providers, analytics providers, and payment processors. We require these providers to use your information only for the purposes of providing services to us and to maintain the confidentiality of your information.
         We may share your information if required by law or if we believe in good faith that such disclosure is necessary to comply with legal processes, enforce our policies, protect our rights or property, or protect the safety and security of our users.
         Your choices:
-
+    </p>
+    <p>
         You can choose to delete your account and personal information at any time by contacting us at mark.dnb@live.nl.
         You can choose not to provide certain personal information, but this may limit your ability to use certain features of our chat app.
         You can choose to disable push notifications or limit the collection of device information in your device settings.
         Security:
-
+    </p> 
+    <p>
         We use commercially reasonable security measures to protect your personal information from unauthorized access, use, or disclosure. However, no data transmission over the Internet or storage system can be guaranteed to be 100% secure.
         Changes to this Privacy Policy:
-
+    </p>
+    <p>
         We may update this Privacy Policy from time to time. If we make significant changes, we will notify you by email or by posting a notice on our chat app or website.
         Contact us:
-
+    </p>
+    <p>
         If you have any questions or concerns about this Privacy Policy or our practices, please contact us at mark.dnb@live.nl.
 
     </p>
