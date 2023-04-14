9094fcb996dabd986215ad3948c3300fd22a690b
mark.dnb@live.nl <mark.dnb@live.nl>
2023-04-14 18:35:06 +0200

    Updating styling of message box

--- pages/messages.vue
+++ pages/messages.vue
@@ -3,12 +3,13 @@
 
       <div ref="messagesBlock" v-for="messageList in messages" :key="messageList.page">
 
-        <div v-for="message, index in messageList.messages" :key="index" class="messages flex py-2">
+        <div v-for="message, index in messageList.messages" :key="index" class="messages flex py-2 px-4">
           <div class="sender flex-0 max-w-xs p-1">
             <ProfilePicture :scale=".65"></ProfilePicture>
           </div>
-          <div class="flex message divide-white bg-white flex-0 p-1 rounded-full">
-            <p class="message-text text-black">
+          <div class="flex message divide-white flex-0 p-1 w-full ">
+            <div class="triangle-conversation border-solid border-r-black border-r-[12px] border-y-transparent border-y-4 border-l-0 h-min mt-11"></div>
+            <p class="message-text text-white bg-black w-full">
               {{ message.message }}
             </p>
           </div>
