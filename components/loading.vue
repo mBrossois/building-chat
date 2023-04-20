e5ea931e48b46532e8364e26f9dac77da2262026
mark.dnb@live.nl <mark.dnb@live.nl>
2023-04-20 16:46:25 +0200

    Maybe fix for created_at not existing?

---
+++ components/loading.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="w-9/12 h-10 mt-5 bg-header-color rounded-full overflow-hidden">
+        <div class="w-5/12 h-full bg-light-pink-color rounded-full "></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
 module.exports = {
     theme: {
-      colors: {
-        // Configure your color palette here
-        'light-pink-color': '#FDB0D4',
-        'header-color': '#4D1831',
-        'main-color': '#B13871',
-        'background-color': '#FAF4E6',
-        'lila-color': '#E4C1F9',
-        'white': '#FFFFFF',
-        'black': '#000000',
-        'window-bg': '#EDF3FF',
-        'transparent': '#00000000'
+      extend: {
+        colors: {
+          // Configure your color palette here
+          'light-pink-color': '#FDB0D4',
+          'header-color': '#4D1831',
+          'main-color': '#B13871',
+          'background-color': '#FAF4E6',
+          'lila-color': '#E4C1F9',
+          'window-bg': '#EDF3FF',
+        },
+        animation: {
+          'line-loading': 'line-loading 1s linear infinite'
+        },
+        keyframes: {
+          'line-loading': {
+            '0%, 100%': { transform: 'rotate(-45deg)' },
+            '50%': { transform: 'rotate(45deg)' },
+          }
+        }
       }
+      
     }
   }
