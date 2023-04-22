9c198b81aed356f5593923e3907b38987145747c
Mark  de Neut Brossois <mark.dnb@live.nl>
2023-04-22 12:57:20 +0000

    setting up gitpod

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
   const user = useSupabaseUser()
-  if (!user.value && !(to.path === '/login' || to.path === '/email-send')) {
+  if (!user.value && !(to.path === '/login' || to.path === '/email-send') && process.env.NODE_ENV === "production") {
     return navigateTo('/login')
   }
 })
